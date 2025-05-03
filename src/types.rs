fbc3332d0dc8afa99a59b013203177bfa2a1177a
Alim Weber <alim.weber@outlook.com>
2025-05-03 18:20:53 +0200

    better internal wiring for types

--- src/types.rs
+++ src/types.rs
@@ -1,43 +1,64 @@
-use crate::{Expression, Error, Type};
-use std::sync::{Arc, Mutex};
+use crate::{Expression, Error, Type, Token, TokenType};
+
+use std::cell::RefCell;
+use std::rc::Rc;
 use std::collections::HashMap;
 
+
 pub struct TypeEnvironment {
+    //this is where assignments go to, be it consts or lets, it does not matter since reassignment
+    //is not possible across types (except for any)
     values : HashMap<String, Type>,
-    enclosing : Option<Arc<Mutex<TypeEnvironment>>>
+
+    //this is where stuff starting with type something goes to
+    types : HashMap<String, Type>,
+    //consts cannot be changed
+
+    enclosing : Option<Rc<RefCell<TypeEnvironment>>>,
+    //this shows all possible mappings inside the current environment, because yes, even
+    //overloadings are not global,
+    //we always have this : (Operation, Type, Type) -> Type, that shows the operation that takes
+    //type x type -> type
+    overloadings : HashMap<(Token, Type, Type), Type>
 }
 
 impl TypeEnvironment{
     //returns a new environemnt that is enclosed in the old one (is "nested" inside)
     pub fn enclose(&mut self, enclosing : TypeEnvironment) -> TypeEnvironment{  
+        
         Self{
-            values : HashMap::new(),
-            enclosing : Some(Arc::new(Mutex::new(enclosing)))
+            enclosing : Some(Rc::new(RefCell::new(enclosing))),
+            ..Default::default()
         } 
     } 
 
     pub fn new() -> Self{
         return Self{
-            values : HashMap::new(),
-            enclosing : None
-        } 
+            ..Default::default()
+        }
     }
 
-    //this puts a new type into the current environment
-    pub fn assign_type(&mut self, key : String, assign_type : Type) -> Result<(), Error>{
-        self.values.insert(key, assign_type);
-        Ok(())
-    }
+
    
 
-    //this tries to do the same, but instead checks if the key exists already and checks if the
-    //things are compatible
-    pub fn reassign_type(&mut self, key : String, assign_type : Type) -> Result<(), Error>{
-        self.values.insert(key, assign_type);
+    pub fn assign_type(&mut self, key : String, assign_type : Type) -> Result<(), Error>{
+
+    
+        
         Ok(())
     }
 }
 
+impl Default for TypeEnvironment{
+    fn default() -> Self {
+        Self{
+            enclosing : None,
+            overloadings : HashMap::new(),
+            types : HashMap::new(),
+            values : HashMap::new()
+        }
+    }
+}
 
 pub fn check_types(ast : Vec<Expression>) -> Result<(), Error>{
 
@@ -45,7 +66,7 @@ pub fn check_types(ast : Vec<Expression>) -> Result<(), Error>{
 
 
     for expression in ast{
-         
+        
     } 
 
 
